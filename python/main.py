d082541d736b00e831c60da6ad4935693b294f62
tking40 <kintk11@gmail.com>
2020-05-17 17:45:58 -0700

    - added functionality to addToCart to handle multiple entries of the same ingredient with different units. Added a sortAndPrint member function to newCart to write out shopping list to csv file.

--- python/main.py
+++ python/main.py
@@ -9,25 +9,25 @@ Created on Sun Apr 19 07:31:27 2020
 
 # Filter Warnings
 import warnings
-warnings.filterwarnings('ignore')
+warnings.filterwarnings("ignore")
 
 import pandas as pd
 import numpy as np
 
-table_path = 'C:/Users/Thomas/Documents/MATLAB/shopping_list/python/tables/'
-recipe_path = 'C:/Users/Thomas/Documents/MATLAB/shopping_list/python/recipes/'
+table_path = "C:/Users/Thomas/Documents/MATLAB/shopping_list/python/tables/"
+recipe_path = "C:/Users/Thomas/Documents/MATLAB/shopping_list/python/recipes/"
 
-stopfoods = pd.read_csv(table_path + 'stop_foods.txt')
-pantry = pd.read_csv(table_path + 'pantry.csv')
-recipe_list = pd.read_csv(table_path + 'recipe_list.csv')
-default_cart = pd.read_pickle(table_path + 'shopping_cart.pickle')
-generic_names = pd.read_csv(table_path + 'generic_names.csv')
-grocery_units = pd.read_csv(table_path + 'grocery_units.csv')
+stopfoods = pd.read_csv(table_path + "stop_foods.txt")
+pantry = pd.read_csv(table_path + "pantry.csv")
+recipe_list = pd.read_csv(table_path + "recipe_list.csv")
+default_cart = pd.read_pickle(table_path + "shopping_cart.pickle")
+generic_names = pd.read_csv(table_path + "generic_names.csv")
+grocery_units = pd.read_csv(table_path + "grocery_units.csv")
 
-v2v_table = pd.read_csv(table_path + 'volume_to_volume.csv')
-m2m_table = pd.read_csv(table_path + 'mass_to_mass.csv')
-v2m_table = pd.read_csv(table_path + 'volume_to_mass.csv')
-m2v_table = pd.read_csv(table_path + 'mass_to_volume.csv')
+v2v_table = pd.read_csv(table_path + "volume_to_volume.csv")
+m2m_table = pd.read_csv(table_path + "mass_to_mass.csv")
+v2m_table = pd.read_csv(table_path + "volume_to_mass.csv")
+m2v_table = pd.read_csv(table_path + "mass_to_volume.csv")
 
 #%% Define Classes and Functions
 class newCart:
@@ -36,29 +36,50 @@ class newCart:
         
     def addToCart(self,newIngredient):
         # check if already in cart
-        idx = self.list.Name == newIngredient.Name
-        if np.any(idx):
-            self.list.Amount[idx] += newIngredient.Amount
-            self.list.Recipe[idx] += ', ' + newIngredient.Recipe
+        nameIdx = self.list.Name == newIngredient.Name
+        if np.sum(nameIdx) == 1: # if == 1, then we have 1 match. 
+            if (self.list.Unit[nameIdx] == newIngredient.Unit).bool():
+                # ^ check for matching units
+                self.list.Amount[nameIdx] += newIngredient.Amount
+                self.list.Recipe[nameIdx] += ", " + newIngredient.Recipe
+            else: # otherwise add separate entry
+                self.list = self.list.append(newIngredient)
+        elif np.sum(nameIdx) > 1: # if multiple matches, then we have an ingredient
+            # with multiple entries due to differing units. 
+            unitIdx = self.list.Unit == newIngredient.Unit
+            matchingIdx = nameIdx & unitIdx
+            if np.any(matchingIdx): #Try to add together any entries with 
+                # matching units
+                if np.sum(matchingIdx) > 1: # we've really messed up
+                    raise ValueError("unexpected value for matchingIdx")
+                self.list.Amount[matchingIdx] += newIngredient.Amount
+                self.list.Recipe[matchingIdx] += ", " + newIngredient.Recipe
+            else: # otherwise add another entry
+                self.list = self.list.append(newIngredient)
         else:
             self.list = self.list.append(newIngredient)
     
     def returnIngredientAmount(self,ingredient_name):
         this_ing = pd.DataFrame.squeeze(self.list[self.list.Name == ingredient_name])
-        print(this_ing.Amount,this_ing.Unit,'of',this_ing.Name)
+        print(this_ing.Amount,this_ing.Unit,"of",this_ing.Name)
+        
+    def sortAndPrint(self):
+        self.list = self.list.sort_values(by=["Name"])
+        self.list = self.list.reset_index(drop=True)
+        self.list.to_csv("shopping_list.csv", index=False)
 
 def convertGenericNames(ingredients,generic_names):
-    ing_list = ingredients['Name']
-    name_list = generic_names['Name']
-    gen_list = generic_names['Generic']
+    ing_list = ingredients["Name"]
+    name_list = generic_names["Name"]
+    gen_list = generic_names["Generic"]
 
     # finding matching indices first prevents from having to loop through every
     # ingredient and check it against the generic list - O(n) vs O(n^2)
     idx = np.where(ing_list.isin(name_list))
     if np.size(idx[0]):
-        for ix in idx:
-            gen_ix = np.where(name_list.isin(ing_list.iloc[ix]))
-            ing_list.iloc[ix[0]] = gen_list.iloc[gen_ix[0][0]]
+        for ix in idx[0]: # this also feels wrong
+            gen_ix = np.where(name_list.isin([ing_list.iloc[ix]]))
+            ing_list.iloc[ix] = gen_list.iloc[gen_ix[0][0]]
             # ^ this is obviously hideous, must be better way to slice and assign.
             # Doing above because np.where returns a tuple with an array inside, and if I 
             # don't pass iloc an integer index, it won't assign correctly to just
@@ -86,7 +107,7 @@ def convertUnits(ingredient,toUnit):
 
 def loadAndFilterRecipe(recipe_name):
     ''' Load'''
-    ingredients = pd.read_csv(recipe_path + recipe_name + '.csv')
+    ingredients = pd.read_csv(recipe_path + recipe_name + ".csv")
     
     ''' Filter '''
     # force lower case for comparisons later
@@ -106,7 +127,7 @@ def loadAndFilterRecipe(recipe_name):
                 ingredients.Amount[index] = convertUnits(ing,des_unit)
                 ingredients.Unit[index] = des_unit
             except:
-                print(ing.Name + " needs special conversion")
+                print(ing.Name,"needs special conversion")
             
     
     #Add recipe name
@@ -130,8 +151,5 @@ for index,recipe in active_recipes.iterrows():
         # do comparison with existing list, then add to cart appropriately
         shopping_cart.addToCart(ingredient)
 
-# Sort and reindex shopping list
-shopping_list = shopping_cart.list.sort_values(by=["Name"])
-shopping_list = shopping_list.reset_index(drop=True)
-
-print(shopping_list)
+#%% Print out list
+shopping_cart.sortAndPrint()
