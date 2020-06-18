deffb920513e070bcfdf7e911a617dffee118606
tking40 <kintk11@gmail.com>
2020-06-18 12:03:06 -0700

    Added new recipes, made updates to generic units and names tables.

--- python/main.py
+++ python/main.py
@@ -35,6 +35,12 @@ m2m_table = pd.read_csv(table_path + "mass_to_mass.csv")
 v2m_table = pd.read_csv(table_path + "volume_to_mass.csv")
 m2v_table = pd.read_csv(table_path + "mass_to_volume.csv")
 
+# verbose = True
+verbose = False
+
+dbug = True
+# dbug = False
+
 #%% Define Classes and Functions
 class newCart:
     def __init__(self, newCart):
@@ -70,12 +76,18 @@ class newCart:
         print(this_ing.Amount,this_ing.Unit,"of",this_ing.Name)
         
     def sortAndPrint(self):
-        self.list = self.list.sort_values(by=["Name"])
+        # sort by name and then by category, so you have items listed 
+        # alphabetically within each category
+        self.list = self.list.sort_values(by=["Name"]) # sort by name
+        self.list = self.list.sort_values(by=['Category']) # sort by category
         self.list = self.list.reset_index(drop=True)
         self.list.to_csv("shopping_list.csv", index=False)
+    def createDefaultCart(self,colList,fname):
+        print("Saving new default cart as",fname)
+        pd.to_pickle(pd.DataFrame(columns = colList),fname)
 
-def convertGenericNames(ingredients,generic_names):
-    ing_list = ingredients["Name"]
+def convertGenericNames(ingredients,generic_names,attr):
+    ing_list = ingredients[attr]
     name_list = generic_names["Name"]
     gen_list = generic_names["Generic"]
 
@@ -92,63 +104,77 @@ def convertGenericNames(ingredients,generic_names):
             # the variable in the series at that index
 
 def convertUnits(ingredient,toUnit):
-    # check if ingredient name is in special table, and if the unit we desire
-    # matches the special table unit
-    m2v = np.any(m2v_table.Name == ingredient.Name)
-    v2m = np.any(v2m_table.Name == ingredient.Name)
-    fromMass = np.any(m2m_table.FromUnits == ingredient.Unit) #compare ingredient unit to mass units
-    if m2v and fromMass:
-        if (m2v_table[m2v_table.Name == ingredient.Name].ToUnit == toUnit).bool():
+    # find why type of units we're converting between (mass or volume)
+    fromMass = np.any(m2m_table.ToUnits == ingredient.Unit)
+    fromVol = np.any(v2v_table.ToUnits == ingredient.Unit)
+    toMass = np.any(m2m_table.ToUnits == toUnit)
+    toVol = np.any(v2v_table.ToUnits == toUnit)
+    if fromMass and toMass: # convert between masses
+        conv_table = m2m_table
+    elif fromVol and toVol: # convert between volumes
+        conv_table = v2v_table
+    elif fromVol and not toVol: # special convert from volume
+        if (v2m_table[v2m_table.Name == ingredient.Name].ToUnits == toUnit).bool():
+            print("Converting",ingredient.Name,"from",ingredient.Unit,"to",toUnit) if verbose else ...
+            return float(v2m_table[v2m_table.Name == ingredient.Name][ingredient.Unit])*ingredient.Amount
+        else:
+            # we have the ingredient listed in the special conversion table,
+            # but the toUnit in the table doesn't match the toUnit passed in to
+            # this function. Raise our own error because there is no KeyError otherwise
+            raise ValueError("unknown special unit conversion")
+    else: # special convert from mass
+        if (m2v_table[m2v_table.Name == ingredient.Name].ToUnits == toUnit).bool():
             print("Converting",ingredient.Name,"from",ingredient.Unit,"to",toUnit)
             return float(m2v_table[m2v_table.Name == ingredient.Name][ingredient.Unit])*ingredient.Amount
         else:
             # we have the ingredient listed in the special conversion table,
             # but the toUnit in the table doesn't match the toUnit passed in to
-            # this function
+            # this function. Raise our own error because there is no KeyError otherwise
             raise ValueError("unknown special unit conversion")
-    elif v2m and not fromMass:
-        if (v2m_table[v2m_table.Name == ingredient.Name].ToUnit == toUnit).bool():
-            print("Converting",ingredient.Name,"from",ingredient.Unit,"to",toUnit)
-            return float(v2m_table[v2m_table.Name == ingredient.Name][ingredient.Unit])*ingredient.Amount
-        else:
-            # we have the ingredient listed in the special conversion table,
-            # but the toUnit in the table doesn't match the toUnit passed in to
-            # this function
-            raise ValueError("unknown special unit conversion")
-    else:
-        # check if mass or volume ingredient
-        if fromMass:                # mass to mass
-            conv_table = m2m_table
-        else:                       # volume to volume
-            conv_table = v2v_table
-        
-        return float(conv_table[conv_table.FromUnits == ingredient.Unit][toUnit])*ingredient.Amount
-        # if the toUnit is a special unit that we haven't listed for this ingredient yet,
-        # then the above line will return a key error
+    
+    return float(conv_table[conv_table.ToUnits == toUnit][ingredient.Unit])*ingredient.Amount
+    # if the toUnit is a special unit that we haven't listed for this ingredient yet,
+    # then the above line will return a key error
 
 def loadAndFilterRecipe(recipe_name):
     ''' Load'''
-    ingredients = pd.read_csv(recipe_path + recipe_name + ".csv")
-    
+    fpath = recipe_path + recipe_name + ".csv"
+    ingredients = pd.read_csv(fpath, dtype={'Amount':'float64'})
+    # ^ pd.read_csv will auto-select datatypes unless specified - here a recipe
+    # might be converted to all int64, which would truncate future arithmetic
+
     ''' Filter '''
+    # add category column
+    ingredients["Category"] = np.nan
     # force lower case for comparisons later
     ingredients.Name = ingredients.Name.str.lower()
     # remove stop foods
     ingredients.drop(ingredients[ingredients.Name.isin(stopfoods.Name)].index)
     # convert generic names
-    convertGenericNames(ingredients,generic_names)
+    convertGenericNames(ingredients,generic_names,"Name")
+    # convert generic units
+    convertGenericNames(ingredients,generic_names,"Unit")
     # convert to common units
-    ing_to_check = ingredients[ingredients.Name.isin(grocery_units.Name)]
-    for index,ing in ing_to_check.iterrows():
-        des_unit = grocery_units.Unit[grocery_units.Name == ing.Name].reset_index(drop=True)[0] # this feels like a hacky workaround - having
-        # trouble getting the string value for comparison, instead of a series object (we're just grabbing the 
-        # first object in the series here)
-        if ing.Unit != des_unit:
-            try:
-                ingredients.Amount[index] = convertUnits(ing,des_unit)
-                ingredients.Unit[index] = des_unit
-            except:
-                print(ing.Name,"needs special conversion")
+    # ing_to_check = ingredients[ingredients.Name.isin(grocery_units.Name)]
+    # for index,ing in ing_to_check.iterrows():
+    for index,ing in ingredients.iterrows():
+        try:
+            des_unit = grocery_units.Unit[grocery_units.Name == ing.Name].reset_index(drop=True)[0] # this fe  els like a hacky workaround - having
+            # trouble getting the string value for comparison, instead of a series object (we're just grabbing the 
+            # first object in the series here)
+            
+            # assign category to ingredient
+            category = grocery_units[grocery_units.Name == ing.Name]["Category"].reset_index(drop=True)[0]
+            # ^ again, hacky workaround to get string from series obj
+            ingredients.Category[index] = category
+            if ing.Unit != des_unit:
+                try:
+                    ingredients.Amount[index] = convertUnits(ing,des_unit)
+                    ingredients.Unit[index] = des_unit
+                except:
+                    print(ing.Name,"needs special conversion") if dbug else ...
+        except:
+            print(ing.Name,"needs assigned category ----") if dbug else ...
             
     
     #Add recipe name
@@ -166,7 +192,7 @@ active_ix = np.flatnonzero(recipe_list.Select) # get active recipes
 active_recipes = recipe_list.iloc[active_ix.flatten()]
 
 for index,recipe in active_recipes.iterrows():
-    print("Adding " + recipe.Name + "...")
+    print("Adding " + recipe.Name + "...") if verbose else ...
     ingredients = loadAndFilterRecipe(recipe.Name)
     for index,ingredient in ingredients.iterrows():
         # do comparison with existing list, then add to cart appropriately
