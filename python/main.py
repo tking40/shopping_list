2848e0432d92c909f9c18770baf5574d24a8cec9
Thomas J. King <thomasj.king@Thomass-MacBook.local>
2020-06-21 10:13:50 -0700

    First cut at moving functions to separate utils file. Also first cut at adding URL import functionality. Current state will load url recipe and add to cart with stored recipes, but with incorrect formatting stemming from incomplete url ingredients structure.

--- python/main.py
+++ python/main.py
@@ -5,198 +5,121 @@ Created on Sun Apr 19 07:31:27 2020
 @author: Thomas
 """
 
-''' Imports and Table Loads'''
-
-# Filter Warnings
+# Standard Library Imports
 import warnings
-warnings.filterwarnings("ignore")
+from sys import platform as _platform
 
+
+# Third Party Imports
 import pandas as pd
 import numpy as np
 
-from sys import platform as _platform
-if _platform == "darwin":
+# Local application imports
+import utils
+# for some reason, 'from . import utils' doesn't work
+
+#%% Initialization
+# Filter Warnings
+warnings.filterwarnings("ignore")
+# ^ Doing this because the way I'm currently assigning values to dataframes
+# triggers a warning that I'm writing to an object reference, not a copy, which
+# is what I want to do
+
+# Platform dependent path
+if _platform == "darwin": # Mac
     root = "/Users/thomasj.king/Documents/Python_Scripts/shopping_list/"
-else:
+else: # Windows
     root = "C:/Users/Thomas/Documents/MATLAB/shopping_list/"
 
+# Table Loads
 table_path = root + "python/tables/"
 recipe_path = root + "python/recipes/"
 
-stopfoods = pd.read_csv(table_path + "stop_foods.txt")
 pantry = pd.read_csv(table_path + "pantry.csv")
 recipe_list = pd.read_csv(table_path + "recipe_list.csv")
-default_cart = pd.read_pickle(table_path + "shopping_cart.pickle")
+url_list = pd.read_csv(table_path + "url_list.csv")
+
+stopfoods = pd.read_csv(table_path + "stop_foods.txt")
 generic_names = pd.read_csv(table_path + "generic_names.csv")
 grocery_units = pd.read_csv(table_path + "grocery_units.csv")
+name_tables = [stopfoods,generic_names,grocery_units]
+
+default_cart = pd.read_pickle(table_path + "shopping_cart.pickle")
 
 v2v_table = pd.read_csv(table_path + "volume_to_volume.csv")
 m2m_table = pd.read_csv(table_path + "mass_to_mass.csv")
 v2m_table = pd.read_csv(table_path + "volume_to_mass.csv")
 m2v_table = pd.read_csv(table_path + "mass_to_volume.csv")
+conv_tables = [v2v_table,m2m_table,v2m_table,m2v_table]
 
-# verbose = True
+# Flags
 verbose = False
-
 dbug = True
-# dbug = False
-
-#%% Define Classes and Functions
-class newCart:
-    def __init__(self, newCart):
-        self.list = newCart
-        
-    def addToCart(self,newIngredient):
-        # check if already in cart
-        nameIdx = self.list.Name == newIngredient.Name
-        if np.sum(nameIdx) == 1: # if == 1, then we have 1 match. 
-            if (self.list.Unit[nameIdx] == newIngredient.Unit).bool():
-                # ^ check for matching units
-                self.list.Amount[nameIdx] += newIngredient.Amount
-                self.list.Recipe[nameIdx] += ", " + newIngredient.Recipe
-            else: # otherwise add separate entry
-                self.list = self.list.append(newIngredient)
-        elif np.sum(nameIdx) > 1: # if multiple matches, then we have an ingredient
-            # with multiple entries due to differing units. 
-            unitIdx = self.list.Unit == newIngredient.Unit
-            matchingIdx = nameIdx & unitIdx
-            if np.any(matchingIdx): #Try to add together any entries with 
-                # matching units
-                if np.sum(matchingIdx) > 1: # we've really messed up
-                    raise ValueError("unexpected value for matchingIdx")
-                self.list.Amount[matchingIdx] += newIngredient.Amount
-                self.list.Recipe[matchingIdx] += ", " + newIngredient.Recipe
-            else: # otherwise add another entry
-                self.list = self.list.append(newIngredient)
-        else:
-            self.list = self.list.append(newIngredient)
-    
-    def returnIngredientAmount(self,ingredient_name):
-        this_ing = pd.DataFrame.squeeze(self.list[self.list.Name == ingredient_name])
-        print(this_ing.Amount,this_ing.Unit,"of",this_ing.Name)
-        
-    def sortAndPrint(self):
-        # sort by name and then by category, so you have items listed 
-        # alphabetically within each category
-        self.list = self.list.sort_values(by=["Name"]) # sort by name
-        self.list = self.list.sort_values(by=['Category']) # sort by category
-        self.list = self.list.reset_index(drop=True)
-        self.list.to_csv("shopping_list.csv", index=False)
-    def createDefaultCart(self,colList,fname):
-        print("Saving new default cart as",fname)
-        pd.to_pickle(pd.DataFrame(columns = colList),fname)
-
-def convertGenericNames(ingredients,generic_names,attr):
-    ing_list = ingredients[attr]
-    name_list = generic_names["Name"]
-    gen_list = generic_names["Generic"]
-
-    # finding matching indices first prevents from having to loop through every
-    # ingredient and check it against the generic list - O(n) vs O(n^2)
-    idx = np.where(ing_list.isin(name_list))
-    if np.size(idx[0]):
-        for ix in idx[0]: # this also feels wrong
-            gen_ix = np.where(name_list.isin([ing_list.iloc[ix]]))
-            ing_list.iloc[ix] = gen_list.iloc[gen_ix[0][0]]
-            # ^ this is obviously hideous, must be better way to slice and assign.
-            # Doing above because np.where returns a tuple with an array inside, and if I 
-            # don't pass iloc an integer index, it won't assign correctly to just
-            # the variable in the series at that index
-
-def convertUnits(ingredient,toUnit):
-    # find why type of units we're converting between (mass or volume)
-    fromMass = np.any(m2m_table.ToUnits == ingredient.Unit)
-    fromVol = np.any(v2v_table.ToUnits == ingredient.Unit)
-    toMass = np.any(m2m_table.ToUnits == toUnit)
-    toVol = np.any(v2v_table.ToUnits == toUnit)
-    if fromMass and toMass: # convert between masses
-        conv_table = m2m_table
-    elif fromVol and toVol: # convert between volumes
-        conv_table = v2v_table
-    elif fromVol and not toVol: # special convert from volume
-        if (v2m_table[v2m_table.Name == ingredient.Name].ToUnits == toUnit).bool():
-            print("Converting",ingredient.Name,"from",ingredient.Unit,"to",toUnit) if verbose else ...
-            return float(v2m_table[v2m_table.Name == ingredient.Name][ingredient.Unit])*ingredient.Amount
-        else:
-            # we have the ingredient listed in the special conversion table,
-            # but the toUnit in the table doesn't match the toUnit passed in to
-            # this function. Raise our own error because there is no KeyError otherwise
-            raise ValueError("unknown special unit conversion")
-    else: # special convert from mass
-        if (m2v_table[m2v_table.Name == ingredient.Name].ToUnits == toUnit).bool():
-            print("Converting",ingredient.Name,"from",ingredient.Unit,"to",toUnit)
-            return float(m2v_table[m2v_table.Name == ingredient.Name][ingredient.Unit])*ingredient.Amount
-        else:
-            # we have the ingredient listed in the special conversion table,
-            # but the toUnit in the table doesn't match the toUnit passed in to
-            # this function. Raise our own error because there is no KeyError otherwise
-            raise ValueError("unknown special unit conversion")
-    
-    return float(conv_table[conv_table.ToUnits == toUnit][ingredient.Unit])*ingredient.Amount
-    # if the toUnit is a special unit that we haven't listed for this ingredient yet,
-    # then the above line will return a key error
-
-def loadAndFilterRecipe(recipe_name):
-    ''' Load'''
-    fpath = recipe_path + recipe_name + ".csv"
-    ingredients = pd.read_csv(fpath, dtype={'Amount':'float64'})
-    # ^ pd.read_csv will auto-select datatypes unless specified - here a recipe
-    # might be converted to all int64, which would truncate future arithmetic
-
-    ''' Filter '''
-    # add category column
-    ingredients["Category"] = np.nan
-    # force lower case for comparisons later
-    ingredients.Name = ingredients.Name.str.lower()
-    # remove stop foods
-    ingredients.drop(ingredients[ingredients.Name.isin(stopfoods.Name)].index)
-    # convert generic names
-    convertGenericNames(ingredients,generic_names,"Name")
-    # convert generic units
-    convertGenericNames(ingredients,generic_names,"Unit")
-    # convert to common units
-    # ing_to_check = ingredients[ingredients.Name.isin(grocery_units.Name)]
-    # for index,ing in ing_to_check.iterrows():
-    for index,ing in ingredients.iterrows():
-        try:
-            des_unit = grocery_units.Unit[grocery_units.Name == ing.Name].reset_index(drop=True)[0] # this fe  els like a hacky workaround - having
-            # trouble getting the string value for comparison, instead of a series object (we're just grabbing the 
-            # first object in the series here)
-            
-            # assign category to ingredient
-            category = grocery_units[grocery_units.Name == ing.Name]["Category"].reset_index(drop=True)[0]
-            # ^ again, hacky workaround to get string from series obj
-            ingredients.Category[index] = category
-            if ing.Unit != des_unit:
-                try:
-                    ingredients.Amount[index] = convertUnits(ing,des_unit)
-                    ingredients.Unit[index] = des_unit
-                except:
-                    print(ing.Name,"needs special conversion") if dbug else ...
-        except:
-            print(ing.Name,"needs assigned category ----") if dbug else ...
-            
-    
-    #Add recipe name
-    name_series = pd.Series(recipe_name).repeat(len(ingredients))
-    name_series.name = "Recipe"
-    return pd.DataFrame.join(ingredients,name_series.reset_index(drop=True)) 
 
 
-
-#%% Loop through recipe list and add ingredients to cart
-shopping_cart = newCart(default_cart) # initialize Cart
+#%% Add stored recipes from csv files
+shopping_cart = utils.newCart(default_cart) # initialize Cart
 
 active_ix = np.flatnonzero(recipe_list.Select) # get active recipes
 # ^ np.argwhere does not work with numpy 1.18.1. Using flatnonzero instead
-active_recipes = recipe_list.iloc[active_ix.flatten()]
+
+# active_recipes = recipe_list.iloc[active_ix.flatten()] # this is needed for np.argwhere
+active_recipes = recipe_list.iloc[active_ix]
 
 for index,recipe in active_recipes.iterrows():
     print("Adding " + recipe.Name + "...") if verbose else ...
-    ingredients = loadAndFilterRecipe(recipe.Name)
+    ingredients = utils.loadAndFilterRecipe(recipe.Name,recipe_path,name_tables,
+                                            conv_tables,verbose,dbug)
     for index,ingredient in ingredients.iterrows():
         # do comparison with existing list, then add to cart appropriately
         shopping_cart.addToCart(ingredient)
 
+#%% Add new recipes from URLs
+
+
 #%% Print out list
 shopping_cart.sortAndPrint()
+
+#%% Soup It!
+
+URL = 'https://www.food.com/recipe/shrimp-and-grits-518698'
+URL = 'https://www.food.com/recipe/slow-cooker-chicken-pozole-345614'
+URL = 'https://www.food.com/recipe/quick-and-easy-pizza-dough-117532'
+URL = 'https://www.budgetbytes.com/slow-cooker-chicken-tikka-masala/'
+"""
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+# results = soup.find(id='__layout')
+
+recipe_directions = soup.find("div", {"class": "recipe-layout__directions"})
+recipe_ingredients = soup.find("div", {"class": "recipe-layout__ingredients"})
+
+
+ingredients = [x.get_text().strip()
+               for x in recipe_ingredients.find_all('li')]
+directions = [x.get_text().strip()
+               for x in recipe_directions.find_all('li')]
+"""
+possibleUnits = ["cup","cups","lbs","pounds","tsp","teaspoons","teaspoon","tbsp","tablespoons","tablespoon"]
+for index,recipe in url_list.iterrows():
+    print("Adding " + recipe.Name + "...") if verbose else ...
+    ingList,dirList = utils.loadURL(recipe.Address)
+    ingredients = utils.parseIngredients(ingList,possibleUnits)
+    
+    for index,ingredient in ingredients.iterrows():
+        # do comparison with existing list, then add to cart appropriately
+        shopping_cart.addToCart(ingredient)
+
+# ix = 0
+# for y in ingredients:
+#     ix+=1
+#     print(ix,y)
+
+# print("Ingredients")
+# ingDF = utils.parseIngredients(ingredients,possibleUnits)
+# print(ingDF)
+# print('')
+
+print("Directions")
+for i,j in enumerate(dirList): print(i+1,j)
---
+++ python/utils.py
@@ -0,0 +1,217 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 17 18:58:54 2020
+
+@author: thomasj.king
+"""
+
+
+""" Recipe Utilities"""
+
+import pandas as pd
+import numpy as np
+from fractions import Fraction
+import requests
+from bs4 import BeautifulSoup
+
+class newCart:
+    def __init__(self, newCart):
+        self.list = newCart
+        
+    def addToCart(self,newIngredient):
+        # check if already in cart
+        nameIdx = self.list.Name == newIngredient.Name
+        if np.sum(nameIdx) == 1: # if == 1, then we have 1 match. 
+            if (self.list.Unit[nameIdx] == newIngredient.Unit).bool():
+                # ^ check for matching units
+                self.list.Amount[nameIdx] += newIngredient.Amount
+                self.list.Recipe[nameIdx] += ", " + newIngredient.Recipe
+            else: # otherwise add separate entry
+                self.list = self.list.append(newIngredient)
+        elif np.sum(nameIdx) > 1: # if multiple matches, then we have an ingredient
+            # with multiple entries due to differing units. 
+            unitIdx = self.list.Unit == newIngredient.Unit
+            matchingIdx = nameIdx & unitIdx
+            if np.any(matchingIdx): #Try to add together any entries with 
+                # matching units
+                if np.sum(matchingIdx) > 1: # we've really messed up
+                    raise ValueError("unexpected value for matchingIdx")
+                self.list.Amount[matchingIdx] += newIngredient.Amount
+                self.list.Recipe[matchingIdx] += ", " + newIngredient.Recipe
+            else: # otherwise add another entry
+                self.list = self.list.append(newIngredient)
+        else:
+            self.list = self.list.append(newIngredient)
+    
+    def returnIngredientAmount(self,ingredient_name):
+        this_ing = pd.DataFrame.squeeze(self.list[self.list.Name == ingredient_name])
+        print(this_ing.Amount,this_ing.Unit,"of",this_ing.Name)
+        
+    def sortAndPrint(self):
+        # sort by name and then by category, so you have items listed 
+        # alphabetically within each category
+        self.list = self.list.sort_values(by=["Name"]) # sort by name
+        self.list = self.list.sort_values(by=['Category']) # sort by category
+        self.list = self.list.reset_index(drop=True)
+        self.list.to_csv("shopping_list.csv", index=False)
+    def createDefaultCart(self,colList,fname):
+        print("Saving new default cart as",fname)
+        pd.to_pickle(pd.DataFrame(columns = colList),fname)
+
+def convertGenericNames(ingredients,generic_names,attr):
+    ing_list = ingredients[attr]
+    name_list = generic_names["Name"]
+    gen_list = generic_names["Generic"]
+
+    # finding matching indices first prevents from having to loop through every
+    # ingredient and check it against the generic list - O(n) vs O(n^2)
+    idx = np.where(ing_list.isin(name_list))
+    if np.size(idx[0]):
+        for ix in idx[0]: # this also feels wrong
+            gen_ix = np.where(name_list.isin([ing_list.iloc[ix]]))
+            ing_list.iloc[ix] = gen_list.iloc[gen_ix[0][0]]
+            # ^ this is obviously hideous, must be better way to slice and assign.
+            # Doing above because np.where returns a tuple with an array inside, and if I 
+            # don't pass iloc an integer index, it won't assign correctly to just
+            # the variable in the series at that index
+
+def convertUnits(ingredient,toUnit,conv_tables,verbose):
+    # find what type of units we're converting between (mass or volume)
+    v2v_table,m2m_table,v2m_table,m2v_table = conv_tables # extract tables
+    
+    fromMass = np.any(m2m_table.ToUnits == ingredient.Unit)
+    fromVol = np.any(v2v_table.ToUnits == ingredient.Unit)
+    toMass = np.any(m2m_table.ToUnits == toUnit)
+    toVol = np.any(v2v_table.ToUnits == toUnit)
+    if fromMass and toMass: # convert between masses
+        conv_table = m2m_table
+    elif fromVol and toVol: # convert between volumes
+        conv_table = v2v_table
+    elif fromVol and not toVol: # special convert from volume
+        if (v2m_table[v2m_table.Name == ingredient.Name].ToUnits == toUnit).bool():
+            print("Converting",ingredient.Name,"from",ingredient.Unit,"to",toUnit) if verbose else ...
+            return float(v2m_table[v2m_table.Name == ingredient.Name][ingredient.Unit])*ingredient.Amount
+        else:
+            # we have the ingredient listed in the special conversion table,
+            # but the toUnit in the table doesn't match the toUnit passed in to
+            # this function. Raise our own error because there is no KeyError otherwise
+            raise ValueError("unknown special unit conversion")
+    else: # special convert from mass
+        if (m2v_table[m2v_table.Name == ingredient.Name].ToUnits == toUnit).bool():
+            print("Converting",ingredient.Name,"from",ingredient.Unit,"to",toUnit)
+            return float(m2v_table[m2v_table.Name == ingredient.Name][ingredient.Unit])*ingredient.Amount
+        else:
+            # we have the ingredient listed in the special conversion table,
+            # but the toUnit in the table doesn't match the toUnit passed in to
+            # this function. Raise our own error because there is no KeyError otherwise
+            raise ValueError("unknown special unit conversion")
+    
+    return float(conv_table[conv_table.ToUnits == toUnit][ingredient.Unit])*ingredient.Amount
+    # if the toUnit is a special unit that we haven't listed for this ingredient yet,
+    # then the above line will return a key error
+
+
+def loadAndFilterRecipe(recipe_name,recipe_path,name_tables,conv_tables,verbose,dbug):
+    ''' Extract Tables'''
+    stopfoods,generic_names,grocery_units = name_tables
+    v2v_table,m2m_table,v2m_table,m2v_table = conv_tables
+    ''' Load'''
+    fpath = recipe_path + recipe_name + ".csv"
+    ingredients = pd.read_csv(fpath, dtype={'Amount':'float64'})
+    # ^ pd.read_csv will auto-select datatypes unless specified - here a recipe
+    # might be converted to all int64, which would truncate future arithmetic
+
+    ''' Filter '''
+    # add category column
+    ingredients["Category"] = np.nan
+    # force lower case for comparisons later
+    ingredients.Name = ingredients.Name.str.lower()
+    # remove stop foods
+    ingredients.drop(ingredients[ingredients.Name.isin(stopfoods.Name)].index)
+    # convert generic names
+    convertGenericNames(ingredients,generic_names,"Name")
+    # convert generic units
+    convertGenericNames(ingredients,generic_names,"Unit")
+    # convert to common units
+    # ing_to_check = ingredients[ingredients.Name.isin(grocery_units.Name)]
+    # for index,ing in ing_to_check.iterrows():
+    for index,ing in ingredients.iterrows():
+        try:
+            des_unit = grocery_units.Unit[grocery_units.Name == ing.Name].reset_index(drop=True)[0] # this fe  els like a hacky workaround - having
+            # trouble getting the string value for comparison, instead of a series object (we're just grabbing the 
+            # first object in the series here)
+            
+            # assign category to ingredient
+            category = grocery_units[grocery_units.Name == ing.Name]["Category"].reset_index(drop=True)[0]
+            # ^ again, hacky workaround to get string from series obj
+            ingredients.Category[index] = category
+            if ing.Unit != des_unit:
+                try:
+                    ingredients.Amount[index] = convertUnits(ing,des_unit,conv_tables,verbose)
+                    ingredients.Unit[index] = des_unit
+                except:
+                    print(ing.Name,"needs special conversion") if dbug else ...
+        except:
+            print(ing.Name,"needs assigned category ----") if dbug else ...
+            
+    
+    #Add recipe name
+    name_series = pd.Series(recipe_name).repeat(len(ingredients))
+    name_series.name = "Recipe"
+    return pd.DataFrame.join(ingredients,name_series.reset_index(drop=True)) 
+
+def myIsNumber(x):
+    # this is a goddamn mess and I don't care
+    try:
+        return float(x)
+    except:
+        try:
+            return float(Fraction(x))
+        except:
+            try:
+                return float(Fraction(x.replace(chr(8260),"/")))
+            except:
+                return False
+        
+
+
+def parseIngredients(ingList,possibleUnits):
+    newList = []
+    for ing in ingList:
+        ingSplit = ing.split()
+        amount = 0
+        for ix in range(len(ingSplit)):
+            x = myIsNumber(ingSplit[ix])
+            if x:
+                amount+=x
+            else:
+                name = ' '.join(ingSplit[ix:])
+                break
+        thisUnit = name.split()[0].lower()
+        if thisUnit in possibleUnits:
+            unit = thisUnit
+            name = ' '.join(name.split()[1:])
+        else:
+            unit = "units"
+        newList.append([name,amount,unit])
+    return pd.DataFrame(newList,columns={"Name","Amount","Unit"})
+
+def loadURL(URL):
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    
+    if "food.com" in URL:
+        recipe_directions = soup.find("div", {"class": "recipe-layout__directions"})
+        recipe_ingredients = soup.find("div", {"class": "recipe-layout__ingredients"})
+    elif "budgetbytes" in URL:
+        recipe_ingredients = soup.find("div", {"class": "wprm-recipe-ingredient-group"})
+        recipe_directions = soup.find("div", {"class": "wprm-recipe-instruction-group"})
+        
+    ingredients = [x.get_text().strip()
+               for x in recipe_ingredients.find_all('li')]
+    directions = [x.get_text().strip()
+               for x in recipe_directions.find_all('li')]
+
+    return ingredients,directions
+
